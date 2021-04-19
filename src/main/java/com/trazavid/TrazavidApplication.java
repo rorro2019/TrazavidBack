c9080ff6461dc56bce38c1760d7572cf8a0a5fbd
Externo - Zalazar Rodrigo Sebastian <rodrigo.zalazar@nbch.com.ar>
2021-04-19 14:38:06 -0300

    first commit

---
+++ src/main/java/com/trazavid/TrazavidApplication.java
@@ -0,0 +1,13 @@
+package com.trazavid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrazavidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrazavidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trazavid/TrazavidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trazavid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrazavidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
