fb32dbd2149653e5921ff1f30a653241b350c3c5
Externo - Zalazar Rodrigo Sebastian <rodrigo.zalazar@nbch.com.ar>
2021-08-04 17:36:37 -0300

    cambiosssss

--- src/main/java/com/trazavid/Controller/AsignaturaController.java
+++ src/main/java/com/trazavid/Controller/AsignaturaController.java
@@ -1,12 +1,56 @@
 package com.trazavid.Controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.trazavid.Entity.Alumno;
+import com.trazavid.Entity.Asignatura;
+import com.trazavid.Service.AlumnoService;
+import com.trazavid.Service.AsignaturaService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/asignatura")
 @CrossOrigin("*")
 public class AsignaturaController {
+    @Resource
+    private AsignaturaService asignaturaService;
+
+
+    //create a new line
+    @PostMapping(value="/new")
+    public ResponseEntity<?> create (@Valid @RequestBody Asignatura asignatura){
+        return ResponseEntity.status(HttpStatus.CREATED).body(asignaturaService.save(asignatura));
+
+    }
+
+    @GetMapping(value = "/all")
+    public Iterable<Asignatura> getAll() {
+        return asignaturaService.findAll();
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> read (@PathVariable(value = "id") Long asignaturaId){
+        Optional<Asignatura> oAlumno= asignaturaService.findById(asignaturaId);
+        if(!oAlumno.isPresent()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(oAlumno);
+    }
+
+    // Delete an User
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete (@PathVariable(value= "id") Long asignaturaId) {
+        if(!asignaturaService.findById(asignaturaId).isPresent()){
+            return ResponseEntity.notFound().build();
+        }
+        asignaturaService.deleteById(asignaturaId);
+        return ResponseEntity.ok().build();
+    }
+
 
 }
