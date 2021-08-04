3d4f9d6c4e4148bddde58b14b5e51a83cfca5fbf
Externo - Zalazar Rodrigo Sebastian <rodrigo.zalazar@nbch.com.ar>
2021-08-03 22:56:55 -0300

    cambiosssss

--- src/main/java/com/trazavid/Controller/AuthController.java
+++ src/main/java/com/trazavid/Controller/AuthController.java
@@ -30,7 +30,7 @@ import java.util.stream.Collectors;
 
 @CrossOrigin("*")
 @RestController
-@RequestMapping("/api/auth/")
+@RequestMapping("api/auth/")
 public class AuthController {
 	@Autowired
     AuthenticationManager authenticationManager;
