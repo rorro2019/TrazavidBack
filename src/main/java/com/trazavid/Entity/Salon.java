867cfeda7d8b8fd3cab53fe376c86feab8ef8ebf
Externo - Zalazar Rodrigo Sebastian <rodrigo.zalazar@nbch.com.ar>
2021-08-04 22:14:22 -0300

    cambi

--- src/main/java/com/trazavid/Entity/Salon.java
+++ src/main/java/com/trazavid/Entity/Salon.java
@@ -19,4 +19,44 @@ public class Salon {
     private String nombre;
 
 
+    public Salon(Integer capacidad, String descripcion, String nombre) {
+        this.capacidad = capacidad;
+        this.descripcion = descripcion;
+        this.nombre = nombre;
     }
+
+    public Salon() {
+    }
+
+    public Long getId_salon() {
+        return id_salon;
+    }
+
+    public void setId_salon(Long id_salon) {
+        this.id_salon = id_salon;
+    }
+
+    public Integer getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(Integer capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
