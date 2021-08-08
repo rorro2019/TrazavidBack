cdbe4fabdc8dc13ea24dabf1a7ec646e70d2dc57
Externo - Zalazar Rodrigo Sebastian <rodrigo.zalazar@nbch.com.ar>
2021-08-08 20:30:04 -0300

    cambios

--- src/main/java/com/trazavid/Entity/Alumno.java
+++ src/main/java/com/trazavid/Entity/Alumno.java
@@ -49,6 +49,8 @@ public class Alumno {
     @Column(length= 15)
     private Long contactoTutor;
 
+
+
     public Alumno(String nombre, String apellido, Date fechaNacimiento, Integer dni, String direccion, String ciudad, String provincia, Long telefono, @NotBlank @Size(max = 50) @Email String email, String nombreTutor, Long contactoTutor) {
         this.nombre = nombre;
         this.apellido = apellido;
