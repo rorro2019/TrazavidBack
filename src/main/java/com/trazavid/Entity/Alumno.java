90767291c17a619ea54b1d37220a902909d98b4b
Externo - Zalazar Rodrigo Sebastian <rodrigo.zalazar@nbch.com.ar>
2021-08-08 19:53:51 -0300

    cambios

--- src/main/java/com/trazavid/Entity/Alumno.java
+++ src/main/java/com/trazavid/Entity/Alumno.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.sql.Date;
 
 @Entity
 @Table(name ="Alumno")
@@ -48,4 +49,116 @@ public class Alumno {
     @Column(length= 15)
     private Long contactoTutor;
 
+    public Alumno(String nombre, String apellido, Date fechaNacimiento, Integer dni, String direccion, String ciudad, String provincia, Integer telefono, @NotBlank @Size(max = 50) @Email String email, String nombreTutor, Long contactoTutor) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimiento = fechaNacimiento;
+        this.dni = dni;
+        this.direccion = direccion;
+        this.ciudad = ciudad;
+        this.provincia = provincia;
+        this.telefono = telefono;
+        this.email = email;
+        this.nombreTutor = nombreTutor;
+        this.contactoTutor = contactoTutor;
+    }
+
+    public Long getId_alumno() {
+        return id_alumno;
+    }
+
+    public void setId_alumno(Long id_alumno) {
+        this.id_alumno = id_alumno;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public Integer getDni() {
+        return dni;
+    }
+
+    public void setDni(Integer dni) {
+        this.dni = dni;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public Integer getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(Integer telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNombreTutor() {
+        return nombreTutor;
+    }
+
+    public void setNombreTutor(String nombreTutor) {
+        this.nombreTutor = nombreTutor;
+    }
+
+    public Long getContactoTutor() {
+        return contactoTutor;
+    }
+
+    public void setContactoTutor(Long contactoTutor) {
+        this.contactoTutor = contactoTutor;
+    }
+
+    public Alumno() {
+    }
 }
