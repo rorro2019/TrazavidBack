9e5a265910881ab2007d0933cb8bfc9adfbdd33d
Externo - Zalazar Rodrigo Sebastian <rodrigo.zalazar@nbch.com.ar>
2021-08-10 19:59:22 -0300

    cambios

--- src/main/java/com/trazavid/Entity/Asignatura.java
+++ src/main/java/com/trazavid/Entity/Asignatura.java
@@ -8,18 +8,43 @@ public class Asignatura {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id_salon;
-
-    @Column(length= 40 )
-    private String objetivo;
+    private Long id_Asignatura;
 
     @Column(length= 40 )
     private String nombre;
 
-    @Column(length= 40 )
-    private String unidades;
-
     @Column(length= 15)
     private Integer carga_horaria;
 
+    public Asignatura() {
+    }
+
+    public Asignatura(String nombre, Integer carga_horaria) {
+        this.nombre = nombre;
+        this.carga_horaria = carga_horaria;
+    }
+
+    public Long getId_Asignatura() {
+        return id_Asignatura;
+    }
+
+    public void setId_Asignatura(Long id_Asignatura) {
+        this.id_Asignatura = id_Asignatura;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getCarga_horaria() {
+        return carga_horaria;
+    }
+
+    public void setCarga_horaria(Integer carga_horaria) {
+        this.carga_horaria = carga_horaria;
+    }
 }
