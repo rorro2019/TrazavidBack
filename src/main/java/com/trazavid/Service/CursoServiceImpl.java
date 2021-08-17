0bf36048369bb4467277412abc2e064aa65967ac
Externo - Zalazar Rodrigo Sebastian <rodrigo.zalazar@nbch.com.ar>
2021-08-17 20:40:25 -0300

    cambios

--- src/main/java/com/trazavid/Service/CursoServiceImpl.java
+++ src/main/java/com/trazavid/Service/CursoServiceImpl.java
@@ -6,9 +6,10 @@ import com.trazavid.Repository.CursoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-
+@Service
 public class CursoServiceImpl implements CursoService {
     @Autowired
     private CursoRepository cursoRepository;
