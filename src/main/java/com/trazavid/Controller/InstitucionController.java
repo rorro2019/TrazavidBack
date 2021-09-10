60dcc807954cc3bf5901d7522796ff17523da12e
Externo - Zalazar Rodrigo Sebastian <rodrigo.zalazar@nbch.com.ar>
2021-09-09 23:06:25 -0300

    ultimos cambios

--- src/main/java/com/trazavid/Controller/InstitucionController.java
+++ src/main/java/com/trazavid/Controller/InstitucionController.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 @RequestMapping("/api/instituciones")
 @CrossOrigin("*")
 public class InstitucionController {
+
     @Resource
     private InstitucionService institucionService;
     @Resource
