108ec9ee0f65d0f87eb63e753a1798664619ffc9
Externo - Zalazar Rodrigo Sebastian <rodrigo.zalazar@nbch.com.ar>
2021-11-02 19:17:22 -0300

    cambios

--- src/main/java/com/trazavid/Service/AsistenciaDocenteServiceImpl.java
+++ src/main/java/com/trazavid/Service/AsistenciaDocenteServiceImpl.java
@@ -7,11 +7,13 @@ import com.trazavid.Repository.AsistenciaDocenteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class AsistenciaDocenteServiceImpl implements AsistenciaDocenteService{
 
     @Autowired
